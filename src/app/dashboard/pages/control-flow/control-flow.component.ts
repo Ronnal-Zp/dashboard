a872346c1e396f725b5cf0807f8429d4ecfdcb24
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-12-01 10:59:38 -0500

    directiva switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A'|'B'|'F';
+
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -9,10 +12,22 @@ import { Component, signal } from '@angular/core';
 })
 export default class ControlFlowComponent {
 
-  public showContent = signal(false);
+  public showContent        = signal(false);
+  public grade              = signal<Grade>('A');
+  public allGrades: Grade[] = ['A','B','F'];
+
 
   public toggleContent() {
     this.showContent.update(value => !value);
+  }
+
+  public generateGrade() {
+    const max = 3;
+    const randomIndex = Math.floor(Math.random() * max);
+
+    const newGrade = this.allGrades[ randomIndex ];
+
+    this.grade.set( newGrade );
   }
 
 }
