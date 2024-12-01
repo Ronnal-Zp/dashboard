e9a88b51e89b0eabcf4d83f37cc7782b49a98444
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-12-01 12:18:31 -0500

    directiva for

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 type Grade = 'A'|'B'|'F';
@@ -6,7 +7,7 @@ type Grade = 'A'|'B'|'F';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
@@ -14,7 +15,10 @@ export default class ControlFlowComponent {
 
   public showContent        = signal(false);
   public grade              = signal<Grade>('A');
+  public frameworks         = signal(['Angular', 'React', 'Laravel', 'Symfony', 'Vue']);
+
   public allGrades: Grade[] = ['A','B','F'];
+  public classBadges = 'flex justify-center rounded-md px-2 py-2 text-xs font-medium ring-1 ring-inset';
 
 
   public toggleContent() {
