c2998e4bd4767c11755bce74a1e86bee88f9cfa1
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-12-01 20:14:55 -0500

    change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
   styleUrl: './change-detection.component.css'
 })
 export default class ChangeDetectionComponent {
+
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    release: 2020
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    release: 2022
+  }
+
+  constructor() {
+    setTimeout(() => {
+        console.log('change!!');
+
+        // this.frameworkAsProperty.name = 'React';
+
+        this.frameworkAsSignal.update(value => {
+          value.name = 'React';
+          return {...value};
+        })
+    }, 2000);
+  }
 
 }
