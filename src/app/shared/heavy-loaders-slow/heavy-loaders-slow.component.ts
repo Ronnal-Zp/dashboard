5e23a917439203330ea86179d99d55be8f29b257
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-12-01 21:39:43 -0500

    defer view

--- src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './heavy-loaders-slow.component.html',
   styleUrl: './heavy-loaders-slow.component.css'
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required: true})
+  cssClass: string = '';
+
+  constructor() {
+    const date = Date.now();
+
+    while (Date.now() - date < 3000) { }
+
+    console.log('Ready!!');
+  }
 
 }
